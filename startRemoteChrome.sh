5c5dcca8be24224cfff8fc2cf0695e6bc190f010
keithgibson <1214624+keithgibson@users.noreply.github.com>
2024-03-01 02:41:57 -0800

    Update startRemoteChrome.sh
    corrected comment

--- startRemoteChrome.sh
+++ startRemoteChrome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Start Chrome with remote debugging and direct the window to port 8000
+# Start Chrome with remote debugging and direct the window to port 8888
 DIR="$(dirname "$0")/CSSxe/Chrome/Profiles"
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
   --remote-debugging-port=9222 \
