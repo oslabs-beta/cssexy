ab4820f39002bca934e0bf36e8e5c025487fcdfe
Mike Basta <mikebasta@gmail.com>
2024-03-15 16:20:22 -0700

    source path added to SidebarStyling component
    Co-authored-by: Keith <1214624+keithgibson@users.noreply.github.com>

--- client/components/RulesAllComp.jsx
+++ client/components/RulesAllComp.jsx
@@ -3,35 +3,36 @@ import { useSelector } from 'react-redux';
 import SidebarStyling from './SidebarStyling.jsx';
 import RulesUserAgentComp from "./RulesUserAgentComp.jsx";
 
-function RulesAllComp(){
+function RulesAllComp() {
     const inlineRules = useSelector(state => state.rules.inlineRules);
     const regularRules = useSelector(state => state.rules.regularRules);
     const styleSheets = useSelector(state => state.rules.styleSheets);
 
-    const RulesInlineComp = inlineRules.map((style, idx) => {
+    const RulesInlineComp = inlineRules.map((each, idx) => {
         // console.log('inlineRules', inlineRules);
-        console.log('styleSheets', styleSheets);
-        console.log(styleSheets[style.rule.styleSheetId]);
-        console.log('styleSheets', styleSheets);
+
         return (
             <SidebarStyling
-            key={`inline-style-${idx}`}
-            selector={style.rule.selectorList?.selectors[0].text}
-            cssProperties={style.rule.style.cssProperties}
-            origin={style.rule.origin}
-            source={styleSheets[style.rule.styleSheetId]}
+                key={`inline-style-${idx}`}
+                selector={each.rule.selectorList?.selectors[0].text}
+                cssProperties={each.rule.style.cssProperties}
+                origin={each.rule.origin}
             />
-            )
-        });
+        )
+    });
 
-    const RulesRegularComp = regularRules.map((style, idx) => {
+    const RulesRegularComp = regularRules.map((each, idx) => {
         // console.log('regularRules', regularRules);
+        console.log('styleSheets', styleSheets);
+        console.log('styleSheetId', each.rule.style.styleSheetId);
+        console.log('styleRule', each.rule);
         return (
             <SidebarStyling
                 key={`regular-style-${idx}`}
-                selector={style.rule.selectorList?.selectors[0].text}
-                cssProperties={style.rule.style.cssProperties}
-                origin={style.rule.origin}
+                selector={each.rule.selectorList?.selectors[0].text}
+                cssProperties={each.rule.style.cssProperties}
+                origin={each.rule.origin}
+                source={styleSheets[each.rule.style.styleSheetId]}
             />
         )
     });
