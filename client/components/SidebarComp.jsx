997e2b489db2680971d9ee53e615472bf47e922c
keithgibson <1214624+keithgibson@users.noreply.github.com>
2024-03-16 21:03:28 -0700

    collapsing sidebar

--- client/components/SidebarComp.jsx
+++ client/components/SidebarComp.jsx
@@ -1,12 +1,34 @@
+import React, { useState } from 'react';
 import RulesAllComp from "./RulesAllComp.jsx";
 
 
 function SidebarComp() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <div className="sidebar" >
-      <h2 style={{textAlign: 'center', marginTop: 0, marginBottom: 0}}>Styles</h2>
-      <RulesAllComp/>
+
+
+    <div className="sidebar-container">
+      <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+        <h2 style={{ textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Styles</h2>
+        <RulesAllComp />
+      </div>
+      <button className="collapse-button" onClick={() => setIsCollapsed(!isCollapsed)}>
+        {isCollapsed ? '>' : '<'}
+      </button>
     </div>
+
+
+
+
+    // <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+    //   <h2 style={{ textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Styles</h2>
+    //   <button onClick={() => setIsCollapsed(!isCollapsed)}>
+    //     Toggle sidebar
+    //   </button>
+    //   <RulesAllComp />
+    // </div>
+
   );
 }
 
